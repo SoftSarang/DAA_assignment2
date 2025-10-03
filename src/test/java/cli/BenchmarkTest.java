12b598bc1f18ac928f9a5fdda900f62aa676e9b3
KapDan04 <dkaparov0@gmail.com>
2025-10-03 19:42:44 +0500

    feat(optimization): measured improvements from object creation optimization

--- src/test/java/cli/BenchmarkTest.java
+++ src/test/java/cli/BenchmarkTest.java
@@ -4,16 +4,15 @@ import algorithms.KadanesAlgorithm;
 import metrics.PerformanceTracker;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
-
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 2)
 public class BenchmarkTest {
 
     @Param({"100", "1000", "10000", "100000"})
@@ -21,19 +20,24 @@ public class BenchmarkTest {
 
     private int[] nums;
 
+
+    private KadanesAlgorithm alg;
+    private PerformanceTracker tracker;
+
     @Setup
     public void setup() {
+        alg = new KadanesAlgorithm();
+        tracker = new PerformanceTracker();
         nums = new int[size];
         Random rand = new Random();
         for (int i = 0; i < size; i++) {
-            nums[i] = rand.nextInt(100) - 50; // Random with negatives
+            nums[i] = rand.nextInt(100) - 50;
         }
     }
 
     @Benchmark
     public void benchmarkKadane(Blackhole blackhole) {
-        KadanesAlgorithm alg = new KadanesAlgorithm();
-        KadanesAlgorithm.SubarrayResult result = alg.findMaxSubarraySum(nums, new PerformanceTracker());
+        KadanesAlgorithm.SubarrayResult result = alg.findMaxSubarraySum(nums, tracker);
         blackhole.consume(result);
     }
 
